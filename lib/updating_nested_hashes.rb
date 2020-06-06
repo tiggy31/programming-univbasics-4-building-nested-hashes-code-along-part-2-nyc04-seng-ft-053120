97e06996d82d0d2aaa64a2e0292836ae90f7f851
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-06 20:12:52 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -63,7 +63,8 @@ def add_characters
    
 
    # Write code below that does the following programmatically:
-   # epic_tragedy[:]
+   # epic_tragedy[:additional_characters] =[]
+   #
    
    
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
