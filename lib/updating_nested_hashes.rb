b34372f05b701e90877dfd35c646cd139b342a8e
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-06 20:28:53 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -65,7 +65,7 @@ def add_characters
    # Write code below that does the following programmatically:
    epic_tragedy[:additional_characters] =[]
    prince = {:name=>"Prince Escalus"}
-   Apothecary = {:name => "Apothecary"}
+   apothecary = {:name=> "Apothecary"}
    epic_tragedy[:additional_characters] << Prince
    epic_tragedy[:additional_characters] << Apothecary
    #For the first hash, assign "Prince Escalus" as the :name
